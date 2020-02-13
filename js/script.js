e3614e5ff13b3c873c81cdcc33373071a10e7cdd
Kamil Bogucki <boguckikamil@hotmail.com>
2020-02-13 16:04:39 +0100

    add function to remove active titles and articles

---
+++ js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/* document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+  }); */
+
+const titleClickHandler = function (event) {
+    console.log('Link was clicked!');
+    console.log(event)
+
+    /* remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+
+    /* add class 'active' to the clicked link */
+
+    /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for(let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
