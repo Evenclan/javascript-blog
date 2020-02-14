2e28ed2b4368c9374f897b3826aec29fee69b8d3
Kamil Bogucki <boguckikamil@hotmail.com>
2020-02-14 11:06:46 +0100

    add 'active' to clicked link

--- js/script.js
+++ js/script.js
@@ -6,10 +6,11 @@
   }); */
 
 const titleClickHandler = function (event) {
-    console.log('Link was clicked!');
-    console.log(event)
+    const clickedElement = this;
+     console.log('Link was clicked!');
+     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,9 +18,13 @@ const titleClickHandler = function (event) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts article.active');
 
