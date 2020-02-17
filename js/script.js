ad34698d261f658c1ff9fb885c2ee03dd51b9b2d
Kamil Bogucki <boguckikamil@hotmail.com>
2020-02-17 09:26:36 +0100

    work on filter function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-'use strict';
+{'use strict';
 
 const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
-
-    console.log('Link was clicked!');
-    //console.log(event);
 
 /* [DONE] remove class 'active' from all article links  */
 
@@ -24,30 +21,64 @@ const titleClickHandler = function (event) {
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles) {
-        if(clickedElement === activeArticle) activeArticle.classList.toggle('active');
-    } 
+        activeArticle.classList.remove('active');
+    }
 
 /* [DONE] get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute('href');
 
-    console.log(articleSelector);
+    console.log(articleSelector); // ID artykułu
 
 /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
     const targetArticle = document.querySelector(articleSelector);
 
-    console.log(targetArticle);
+    console.log(targetArticle); // Cały artykuł
 
 /* add class 'active' to the correct article */
 
     targetArticle.classList.toggle('active');
-    
-    
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
+}
+
+    const optArticleSelector = '.post';
+    const optTitleSelector = '.post-title';
+    const optTitleListSelector = '.titles';
+
+    function generateTitleLinks() {
+
+
+    /* Remove contents of titleList */
+
+    titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = ''
+
+    console.log(titleList);
+    
+
+
+    /* for each article */
+
+        /* get the article id */
+
+        /* fint the title element */
+
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        /* insert link into titleList */
+
+    }
+
+    generateTitleLinks ();
+
+
+
 }
